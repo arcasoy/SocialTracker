b83ede16b96ed61b7647a14960aa69bcf41757b5
arcasoy <arcasoy1@gmail.com>
2020-05-27 02:29:15 +0000

    Initializing DB connection

--- modules/db_modules.js
+++ modules/db_modules.js
@@ -1,21 +1,21 @@
 const mysql = require('mysql');
+const auth = require('../auth/auth.json');
 
 module.exports = {
     insert: async function insert(dbName, table, data) {
-        var con = mysql.createConnection({
-            host: "35.221.52.192",
-            user: "root",
-            password: "bot"
-        })
+        var con = mysql.createConnection(auth.dbLogin)
     
         con.connect(function(err) {
             if (err) throw err;
             console.log("Connected to MySQL Server");
             con.query(`CREATE DATABASE IF NOT EXISTS ${dbName}`, function (err, result) {
                 if (err) throw err;
-                //select database here, none selected.
-                con.query(`CREATE TABLE IF NOT EXISTS ${table}`, function (err, result) {
+                con.query(`USE ${dbName}`, function (err, result) {
                     if (err) throw err;
+                    //A table must have at least 1 column error
+                    con.query(`CREATE TABLE IF NOT EXISTS ${table}`, function (err, result) {
+                        if (err) throw err;
+                    })
                 })
             });
         })
